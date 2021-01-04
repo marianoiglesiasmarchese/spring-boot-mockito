482130a7377246f33e653420d7156200e821c7dd
mariano iglesias marchese <marianoiglesiasmarchese@gmail.com>
2021-01-03 21:38:31 -0300

    base spring boot project

---
+++ src/main/kotlin/com/example/mockito/MockitoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mockito
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MockitoApplication
+
+fun main(args: Array<String>) {
+	runApplication<MockitoApplication>(*args)
+}
